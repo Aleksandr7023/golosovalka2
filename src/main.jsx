8517fb8520bf1d5bf22026cf17b117ae699bb095
Aleksandr7023 <grudnick@mail.ru>
2025-11-12 12:49:11 +0400

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <h1 style={{ textAlign: 'center', marginTop: '100px' }}>
+    ГОЛОСОВАЛКА РАБОТАЕТ!
+  </h1>
+)
