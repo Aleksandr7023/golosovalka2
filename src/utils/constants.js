53a98eeee67bc0db1a2eef22960ceaa0dd8f740f
Aleksandr7023 <grudnick@mail.ru>
2025-11-28 15:59:27 +0400

    bump version v1.28 → v1.29 + обновления (28.11.25 15:59:27)

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 ﻿// src/utils/constants.js — все константы в одном месте
 
 // Версия приложения (меняется только здесь!)
-export const APP_VERSION = 'v1.28'
+export const APP_VERSION = 'v1.29'
 
 // Максимальное количество вариантов ответа
 export const MAX_OPTIONS = 20
