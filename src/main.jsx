aea9ca15af1827a83dc36bf4ddfb3adfdcfba416
Aleksandr7023 <grudnick@mail.ru>
2025-12-30 14:34:19 +0400

    bump version v1.63 → v1.64 (30.12.25 14:34:19)

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
+// main.jsx
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client'; // ← правильный импорт
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-﻿export const APP_VERSION = 'v1.63';
+﻿export const APP_VERSION = 'v1.64';
 export const APP_NAME = 'Голосовалка';
